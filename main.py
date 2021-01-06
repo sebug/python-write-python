0e36f5da7710787277272ea4de56e9e33252d3d3
sebug <sebastian.gfeller@gmail.com>
2021-01-06 20:50:10 +0100

    Printing AST

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 def analyze_file_content(file_content):
-    return file_content
+    tree = ast.parse(file_content)
+    return tree
 
 def analyze_file(f):
     with open(f, 'r') as file:
-        file_content = file.readlines()
+        file_content = file.read()
         analyzed = analyze_file_content(file_content)
         print(analyzed)
 
+
+analyze_file(sys.argv[1])
